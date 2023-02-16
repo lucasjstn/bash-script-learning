e071ebec5ca910e63d649f057339ab1bcf50aa20
lucasjstn <lucas7justino@gmail.com>
2023-02-16 15:00:44 -0600

    using variables

--- 01-helloworld.sh
+++ 01-helloworld.sh
@@ -1,1 +1,6 @@
-ls
+#!/bin/bash
+
+echo "Hello World"
+
+echo "My current working directory is: "
+pwd
---
+++ 02-variables.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+myname="lucas"
+myage="40"
+echo "my name is $myname, and my age is $myage"
